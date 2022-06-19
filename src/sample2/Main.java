a00a6e6089a0a25fef129b069cf737d52fa45255
kazuma-shinomiya <kazuma.s6145@icloud.com>
2022-06-19 16:00:19 +0900

    委譲によるAdapter

---
+++ src/sample2/Main.java
@@ -0,0 +1,12 @@
+package sample2;
+
+import sample1.Print;
+import sample1.PrintBanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Print p = new PrintBanner("Hello");
+        p.printWeak();
+        p.printStrong();
+    }
+}
